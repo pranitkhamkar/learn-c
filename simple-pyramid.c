ad0ceeb3aff8b86cb98cc362f4e621e62b7f2638
Bhalchandra <bsahasrabudhe@gmail.com>
2021-08-11 19:15:03 -0500

    feat: added reverse numbers

--- simple-pyramid.c
+++ simple-pyramid.c
@@ -12,5 +12,14 @@ int main() {
        printf("\n");
    }
 
+   for(i=0;i<10;i++)
+   {
+       for(j=10;j>0;j--)
+       {
+            printf("%d",j);
+       }
+       printf("\n");
+   }
+
    return 0;
 }
