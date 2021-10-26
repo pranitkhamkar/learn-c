f3c856d8162d62e1390b2addfbdca9f05330df94
pranitkhamkar <88722447+pranitkhamkar@users.noreply.github.com>
2021-10-26 15:26:45 +0530

    improvement: variables

--- ArithmeticCwh7.c
+++ ArithmeticCwh7.c
@@ -2,14 +2,16 @@
 
 int main()
 {
-    int a, b ;
+    int a;
+    float b;
+    
     a=34 ;
     b=6 ;
 
-    printf("a + b = %d\n", a+b);
-    printf("a - b = %d\n", a-b);
-    printf("a * b = %d\n", a*b);
-    printf("a / b = %d\n", a/b);
+    printf("a + b = %f\n", a+b);
+    printf("a - b = %df\n", a-b);
+    printf("a * b = %f\n", a*b);
+    printf("a / b = %f\n", a/b);
     
     return 0;
 }
