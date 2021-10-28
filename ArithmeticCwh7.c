bebd7c77ffad1f888ff5f3fd103d55dc7fd75cf7
Pranit Khamkar <pranitbkhamkar@gmail.com>
2021-10-28 17:27:15 +0530

    fix: polish

--- ArithmeticCwh7.c
+++ ArithmeticCwh7.c
@@ -2,15 +2,17 @@
 
 int main()
 {
-    int a, b ;
+    int a;
+    float b;
+    
     a=34 ;
     b=6 ;
 
-    printf("a + b = %d\n", a+b);
-    printf("a - b = %d\n", a-b);
-    printf("a * b = %d\n", a*b);
-    printf("a / b = %d\n", a/b);
-    
+    printf("a + b = %f\n", a+b);
+    printf("a - b = %f\n", a-b);
+    printf("a * b = %f\n", a*b);
+    printf("a / b = %f\n", a/b);
+    //This is trial
     return 0;
 }
 
