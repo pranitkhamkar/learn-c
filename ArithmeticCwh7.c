19bd62d3cc53e4bb51c05f31be41a94f56f905af
Pranit Khamkar <pranitbkhamkar@gmail.com>
2021-10-28 17:32:08 +0530

    feat: demo

--- ArithmeticCwh7.c
+++ ArithmeticCwh7.c
@@ -3,16 +3,27 @@
 int main()
 {
     int a;
+<<<<<<< HEAD
+=======
+    
+>>>>>>> e85c38953d2207d0a97f13b2f68b59a953ee0f8c
     float b;
     
     a=34 ;
     b=6 ;
 
     printf("a + b = %f\n", a+b);
+<<<<<<< HEAD
     printf("a - b = %f\n", a-b);
     printf("a * b = %f\n", a*b);
     printf("a / b = %f\n", a/b);
     //This is trial
+=======
+    printf("a - b = %df\n", a-b);
+    printf("a * b = %f\n", a*b);
+    printf("a / b = %f\n", a/b);
+    
+>>>>>>> e85c38953d2207d0a97f13b2f68b59a953ee0f8c
     return 0;
 }
 
--- Ex1-tables-Cwh8.c
+++ Ex1-tables-Cwh8.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # include <stdio.h>
 /*
 Print multiplication table of a number entered by the user in pretty form
@@ -28,4 +29,36 @@ printf("\ntable of %d as follows: \n\n", n);
     for (int i=1;i<=10;i++){
         printf("%d X %d = %d\n", n, i , n*i );
     }
+=======
+# include <stdio.h>
+/*
+Print multiplication table of a number entered by the user in pretty form
+
+Example:
+
+Input
+Enter the number you want multiplication table of:
+6
+
+Output:
+Table of 6:
+6 X 1 = 6
+6 X 2 = 12
+.
+.
+.
+6 X 10 = 60
+
+*/
+
+int main()
+{
+    int n;
+printf("give the no.: ");
+    scanf("%d", &n);
+printf("\ntable of %d as follows: \n\n", n);
+    for (int i=1;i<=10;i++){
+        printf("%d X %d = %d\n", n, i , n*i );
+    }
+>>>>>>> e85c38953d2207d0a97f13b2f68b59a953ee0f8c
 }
