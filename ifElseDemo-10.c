6693f7e55b9d8f022b799e4d3a1fcbf36c9465a6
Pranit Khamkar <pranitbkhamkar@gmail.com>
2021-10-30 00:03:40 +0530

    feat: if else demo

---
+++ ifElseDemo-10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("Enter your age\n");
+    scanf("%d", &age);
+    printf("You have entered %d as your age\n", age);
+
+    if (age>=18)
+    {
+        printf("you can vote...!");
+    }
+
+    else if(age>=10)
+        printf("You can vote for kids elecctins.");
+    }
+
+    else{
+        printf("you can not vote...!")
+    }]
+
+    return 0;
+}
